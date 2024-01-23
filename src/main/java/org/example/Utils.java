aa1d370a84139e04ec27e3425ed81f25c0f6855f
Louis Doherty <louisdoherty98@gmail.com>
2024-01-23 15:12:00 +0000

    Initial commit of JUnit demo

---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Utils {
+
+    public static boolean isPrime(int num) {
+        for(int i = 2; i < (num/2)+1; i++) {
+            if (num%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String reverse(String toReverse) {
+        String toReturn = "";
+        for(int i = toReverse.length(); i > 0; i--) {
+            toReturn += toReverse.charAt(i);
+        }
+        return toReturn;
+    }
+}
---
+++ src/test/java/UtilsTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.*;
+import static org.example.Utils.isPrime;
+public class UtilsTest {
+
+    @Test
+    public void testRealityStillUnchanged() {
+        Assertions.assertEquals(1,2/2,"Oh god...");
+    }
+
+    @Test
+    public void testIsPrimeSuccess() {
+        Assertions.assertTrue(isPrime(7),"7 found to NOT be prime");
+        Assertions.assertTrue(isPrime(13),"13 found to NOT be prime");
+        Assertions.assertTrue(isPrime(19),"19 found to NOT be prime");
+    }
+
+    @Test
+    public void testIsPrimeFailure() {
+        Assertions.assertFalse(isPrime(4),"4 found to be prime");
+        Assertions.assertFalse(isPrime(10),"10 found to be prime");
+        Assertions.assertFalse(isPrime(1),"1 found to be prime");
+    }
+
+
+}
