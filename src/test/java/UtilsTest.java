c078e2516dd059feac7dc96318cb38aff6c5975c
LouisDoh <risonic@hotmail.co.uk>
2024-02-20 10:35:11 +0000

    Sonarcloud set up

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -7,4 +7,17 @@ public class UtilsTest {
         Assertions.assertEquals(1,2/2,"Oh god...");
     }
 
+    @Test
+    public void testPrimeCorrects() {
+        Assertions.assertTrue(isPrime(7), "7 found not to be prime.");
+        Assertions.assertTrue(isPrime(2), "2 found not to be prime.");
+        Assertions.assertTrue(isPrime(17), "17 found not to be prime.");
+    }
+
+    @Test
+    public void testPrimeFalses() {
+        Assertions.assertFalse(isPrime(6), "6 found to be prime.");
+        Assertions.assertFalse(isPrime(1), "1 found to be prime");
+    }
+
 }
