a1d7d6e1624dc85f96ec2e4f2cf1e9ab74d1a225
Louis Doherty <louisdoherty98@gmail.com>
2024-03-04 16:05:28 +0000

    Really solved the problems this time

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -11,7 +11,6 @@ public class UtilsTest {
     }
 
     @Test
-<<<<<<< HEAD
     public void testIsPrimeTrue() {
         Assertions.assertTrue(isPrime(2), "2 found not to be prime");
         Assertions.assertTrue(isPrime(7), "7 found not to be prime");
@@ -22,17 +21,5 @@ public class UtilsTest {
     public void testIsPrimeFalse() {
         Assertions.assertFalse(isPrime(6), "6 found to be prime");
         Assertions.assertFalse(isPrime(10), "10 found to be prime");
-=======
-    public void testPrimeCorrects() {
-        Assertions.assertTrue(isPrime(7), "7 found not to be prime.");
-        Assertions.assertTrue(isPrime(2), "2 found not to be prime.");
-        Assertions.assertTrue(isPrime(17), "17 found not to be prime.");
-    }
-
-    @Test
-    public void testPrimeFalses() {
-        Assertions.assertFalse(isPrime(6), "6 found to be prime.");
->>>>>>> 62062f94b51c8c3792cc3e63a2ea411fe9a38947
-        //Assertions.assertFalse(isPrime(1), "1 found to be prime");
     }
 }
